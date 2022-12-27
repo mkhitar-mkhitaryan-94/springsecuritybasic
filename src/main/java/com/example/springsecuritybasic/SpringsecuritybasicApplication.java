f4ddba1fb8de625dacc6170103c274fb22859b41
mkhitar <www.mkhitaryan94@mail.ru>
2022-12-27 20:07:17 +0400

    Spring Security update

--- src/main/java/com/example/springsecuritybasic/SpringsecuritybasicApplication.java
+++ src/main/java/com/example/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,14 +2,13 @@ package com.example.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication
-@ComponentScan("com.example.springsecuritybasic.controller")
 public class SpringsecuritybasicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringsecuritybasicApplication.class, args);
-    }
 
+    }
 }
---
+++ src/main/java/com/example/springsecuritybasic/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String saveContactInquiryDetails() {
+        return "Inquiry details are saved to the DB";
+    }
+}
